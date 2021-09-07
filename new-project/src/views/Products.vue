46036c355b3931cc45506bcd07fc6fbd6c94f20c
DESKTOP-QSEF0D3\大哥 <carl12331@gmail.com>
2021-09-07 17:35:35 +0800

    2021/09/07

--- new-project/src/views/Products.vue
+++ new-project/src/views/Products.vue
@@ -1,4 +1,9 @@
 <template>
+  <div class="text-end">
+    <button class="btn btn-primary" type="button" @click="openModal(true)">
+      增加一個產品
+    </button>
+  </div>
   <table class="table mt-4">
     <thead>
       <tr>
@@ -12,47 +17,77 @@
     </thead>
     <tbody>
       <tr v-for="item in products" :key="item.id">
-        <td>{{item.category}}</td>
-        <td>{{item.title}}</td>
-        <td class="text-right">{{item.origin_price}}</td>
-        <td class="text-right">{{item.price}}</td>
+        <td>{{ item.category }}</td>
+        <td>{{ item.title }}</td>
+        <td class="text-right">{{ item.origin_price }}</td>
+        <td class="text-right">{{ item.price }}</td>
         <td>
           <span class="text-success" v-if="item.is_enabled">啟用</span>
           <span class="text-muted" v-else>未啟用</span>
         </td>
         <td>
           <div class="btn-group">
-            <button class="btn btn-outline-primary btn-sm">編輯</button>
+            <button class="btn btn-outline-primary btn-sm" @click="openModal(false,item)">編輯</button>
             <button class="btn btn-outline-danger btn-sm">刪除</button>
           </div>
         </td>
       </tr>
     </tbody>
   </table>
+  <productModal ref="productModal" :product="tempProduct" @update-product="updateProduct"></productModal>
 </template>
 <script>
+import productModal from "../components/ProductModal.vue";
+
 export default {
   data() {
     return {
       products: [],
       pagination: {},
+      tempProduct: {},
+      isNew:false,
     };
+  },
+  components: {
+    productModal,
   },
   methods: {
     getProducts() {
       const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products`;
       this.$http.get(api).then((res) => {
         if (res.data.success) {
-          console.log('products',res.data);
+          console.log("products", res.data);
           this.products = res.data.products;
           this.pagination = res.data.pagination;
-
         }
+      });
+    },
+    openModal(isNew, item) {
+      if (isNew){
+        this.tempProduct = {};        
+      }else{
+        this.tempProduct = {...item};
+        console.log('temp',this.tempProduct);
+      }
+      this.isNew = isNew;
+      console.log(isNew,item);
+      const productComponent = this.$refs.productModal;
+      productComponent.showModal();
+    },
+    updateProduct(item){
+      this.tempProduct = item;
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product`;
+      const productComponent = this.$refs.productModal;
+      this.$http.post(api,{data:this.tempProduct}).
+      then((res)=>{
+        console.log(res);
+        productComponent.hideModal();
+        this.getProducts();
       });
     },
   },
   created() {
-        this.getProducts();
-    },
+    this.getProducts();
+  },
 };
 </script>
