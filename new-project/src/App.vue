ee44be1cd25f0e8e6cb9809e526dabbef8862483
L1uXO330B7 <carl12331.gmail.com>
2022-05-25 13:10:56 +0800

    NAVBAR

--- new-project/src/App.vue
+++ new-project/src/App.vue
@@ -3,5 +3,9 @@
 </template>
 <style lang="scss">
 @import "./assets/all";
-// 引入boostrap 
+// 引入全域SCSS 
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@400;700;900&display=swap');
+body{
+  font-family: 'Noto Serif TC', serif;
+}
 </style>
--- new-project/src/components/Navbar.vue
+++ new-project/src/components/Navbar.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar w/ text</a>
+      <a class="navbar-brand text-center" href="#"
+        ><span class="h1">NEED</span><h5>你的雜貨專賣</h5></a
+      >
       <button
         class="navbar-toggler"
         type="button"
@@ -25,11 +27,20 @@
             <a class="nav-link" href="#" @click.prevent="logout">登出</a>
           </li>
         </ul>
-        <span class="navbar-text"> Navbar text with an inline element </span>
       </div>
     </div>
   </nav>
 </template>
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap');
+.navbar-collapse{
+  flex-grow: 0;
+}
+.h1{
+  font-size: 25px;
+  font-family: 'Rock Salt', cursive;
+}
+</style>
 <script>
 export default {
   methods: {
@@ -37,7 +48,7 @@ export default {
       const api = `${process.env.VUE_APP_API}logout`;
       // 登出功能
       this.$http.post(api).then((res) => {
-        if (res.data.success) { 
+        if (res.data.success) {
           const { token, expired } = res.data;
           this.$router.push("/login");
         }
