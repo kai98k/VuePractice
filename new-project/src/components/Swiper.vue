9f994bd796581b3e143c60fb7672ff927cdb3890
johch3n611u <401450650@gapp.fju.edu.tw>
2022-05-28 21:25:40 +0800

    Fix import

--- new-project/src/components/Swiper.vue
+++ new-project/src/components/Swiper.vue
@@ -1,25 +1,51 @@
 <template>
-  <swiper
-    :slides-per-view="3"
-    :space-between="50"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange"
-  >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    ...
-  </swiper>
+<swiper
+:slides-per-view="4"
+:space-between="30"
+@swiper="onSwiper"
+@slideChange="onSlideChange"
+class="default-slider">
+<swiper-slide v-for="n in 7" :key="n"> {{ n }} </swiper-slide></swiper>
 </template>
-<style lang="scss" scoped>
-@import "";
+<style scoped>
+.default-slider .swiper-slide {
+  display: flex;
+  height: 300px !important;
+  justify-content: center;
+  align-items: center;
+  color: #000;
+  font-size: 24px;
+  font-weight: 700;
+}
+.swiper-slide:nth-child(1n) {
+  background-color: palevioletred;
+}
+.swiper-slide:nth-child(2n) {
+  background-color: skyblue;
+}
+.swiper-slide:nth-child(3n) {
+  background-color: peru;
+}
+.swiper-slide:nth-child(4n) {
+  background-color: cadetblue;
+}
+.swiper-slide:nth-child(5n) {
+  background-color: plum;
+}
+.swiper-slide:nth-child(6n) {
+  background-color: goldenrod;
+}
+.swiper-slide:nth-child(7n) {
+  background-color: palegreen;
+}
 </style>
 <script>
-  // Import Swiper Vue.js components
-  import { Swiper, SwiperSlide } from 'swiper/vue';
 
-  // Import Swiper styles
-  import 'swiper/css';
+// https://blog.canopas.com/implement-different-types-of-sliders-using-vue-3-and-swiper-7-ab79bd5abe28
+
+import { Swiper } from "swiper/vue/swiper";
+import { SwiperSlide } from "swiper/vue/swiper-slide";
+import "swiper/swiper.min.css";
 
   export default {
     components: {
