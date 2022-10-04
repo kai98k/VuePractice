6696c4b2c7e1a2e750edd5db9e7e37e4c6a9ae20
DESKTOP-QSEF0D3\大哥 <carl12331@gmail.com>
2022-10-05 00:04:36 +0800

    order

--- new-project/src/components/Userboard/CartModal.vue
+++ new-project/src/components/Userboard/CartModal.vue
@@ -71,24 +71,114 @@
             </tbody>
           </table>
         </div>
-        <div class="modal-footer" v-if="cartLength>0">
-          <div>
-            <button class="btn btn-outline-dark" type="button" @click="useCoupon">套用優惠</button>
-            <input id="coupon" class="form-control ms-3" type="text" placeholder="輸入優惠券"  v-model="code" style="width:200px"/>
-            <span class="ms-3 text-danger" v-if="!couponFound">*無此優惠券</span>
-            <h5 class="mt-3">總計金額 : {{ cartData.total }} 元</h5>
-            <h5>優惠金額 : {{Math.round(cartData.final_total)}} 元</h5>
-
+        <div class="modal-footer" v-if="cartLength > 0">
+          <div class="wrap">
+            <div>
+              <button
+                class="btn btn-outline-dark"
+                type="button"
+                @click="useCoupon"
+              >
+                套用優惠
+              </button>
+              <input
+                id="coupon"
+                class="form-control ms-3"
+                type="text"
+                placeholder="輸入優惠券"
+                v-model="code"
+                style="width: 200px"
+              />
+              <span class="ms-3 text-danger" v-if="!couponFound"
+                >*無此優惠券</span
+              >
+              <h5 class="mt-3">總計金額 : {{ cartData.total }} 元</h5>
+              <h5>優惠金額 : {{ Math.round(cartData.final_total) }} 元</h5>
+            </div>
+            <div>
+              <button
+                type="button"
+                class="btn btn-danger me-3"
+                @click="deleteAllCarts"
+                v-if="!IsOrdered"
+              >
+                清空購物車
+              </button>
+              <button
+                type="button"
+                class="btn btn-success me-3"
+                v-if="IsOrdered"
+                @click="SendOrder"
+              >
+                確定下單
+              </button>
+              <button
+                class="btn btn-primary"
+                @click="openOrder"
+                v-if="!IsOrdered"
+              >
+                前往下單
+              </button>
+              <button
+                class="btn btn-danger"
+                @click="openOrder"
+                v-if="IsOrdered"
+              >
+                取消下單
+              </button>
+            </div>
           </div>
-          <div>
-            <button
-              type="button"
-              class="btn btn-danger me-3"
-              @click="deleteAllCarts"
-            >
-              清空購物車
-            </button>
-            <router-link to="/userorder" class="btn btn-primary" @click="hideModal">前往下單</router-link>
+          <div v-if="IsOrdered" class="order">
+            <div>
+              <h5 class="text-center">下單資料</h5>
+              <label for="name" class="form-label">姓名</label>
+              <input
+                v-model="user.name"
+                type="text"
+                class="form-control"
+                id="name"
+                placeholder="下單者姓名"
+                required
+              />
+              <label for="email" class="form-label mt-2">電子郵件</label>
+              <input
+                v-model="user.email"
+                type="email"
+                class="form-control"
+                id="email"
+                placeholder="email@.com"
+                required
+              />
+              <label for="tel" class="form-label mt-2">手機號碼</label>
+              <input
+                v-model="user.tel"
+                type="tel"
+                class="form-control"
+                id="tel"
+                placeholder="您的手機號碼"
+                required
+              />
+              <label for="address" class="form-label mt-2">地址</label>
+              <input
+                v-model="user.address"
+                type="text"
+                class="form-control"
+                id="address"
+                placeholder="您的寄送地址"
+                required
+              />
+              <label for="message" class="form-label mt-2">留言</label>
+              <textarea
+                id="message"
+                rows="5"
+                cols="33"
+                class="form-control"
+                v-model="message"
+              >
+您的特殊需求...
+</textarea
+              >
+            </div>
           </div>
         </div>
       </div>
@@ -110,7 +200,20 @@
 .modal-footer {
   border: 0;
   padding: 10px 25px;
-  justify-content: space-between;
+  display: block;
+  .wrap {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .form-control {
+    display: inline-block;
+    width: 100%;
+  }
+  .order {
+    display: flex;
+    justify-content: center;
+  }
 }
 .cartImg {
   width: 200px;
@@ -165,8 +268,8 @@ export default {
   },
   data() {
     return {
-      couponFound:true,
-      code:"",
+      couponFound: true,
+      code: "",
       cartLength: 0,
       isLoading: false,
       modal: {},
@@ -174,21 +277,28 @@ export default {
       status: {
         loading: "",
       },
-      // 變數拿來裝 ref 取到 modal DOM
+      IsOrdered: false,
+
+      user: {
+        name: "",
+        email: "",
+        tel: "",
+        address: "",
+      },
+      message: "",
     };
   },
   methods: {
-    useCoupon(){
-       let data = {
-         code:this.code,
-       }
-       const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/coupon`;
-       this.$http.post(api,{data})
-        .then((res) => {
-          this.couponFound = res.data.success;
-          this.cartData.final_total = res.data.data.final_total;
-          console.log("coupon",res);
-        })
+    useCoupon() {
+      let data = {
+        code: this.code,
+      };
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/coupon`;
+      this.$http.post(api, { data }).then((res) => {
+        this.couponFound = res.data.success;
+        this.cartData.final_total = res.data.data.final_total;
+        console.log("coupon", res);
+      });
     },
     deleteAllCarts() {
       this.isLoading = true;
@@ -209,7 +319,6 @@ export default {
       this.isLoading = false;
     },
     updateCart(item) {
-      console.log(item);
       this.status.loading = item.id;
       let data = {
         product_id: item.id,
@@ -225,12 +334,31 @@ export default {
         }
       });
     },
+    SendOrder() {
+      let data = {
+        user: this.user,
+        message: this.message,
+      };
+      console.log("orderData",data);
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/order`;
+      this.$http.post(api, { data }).then((res) => {
+        console.log("res", res);
+        if (res.data.success) {
+          this.hideModal();
+          this.$router.push('/userorder')
+        }
+      });
+    },
     showModal() {
       this.modal.show();
       console.log(this.cartData);
     },
     hideModal() {
       this.modal.hide();
+    },
+    openOrder() {
+      console.log(this.IsOrdered);
+      this.IsOrdered = !this.IsOrdered;
     },
   },
   mounted() {
--- new-project/src/views/UserOrder.vue
+++ new-project/src/views/UserOrder.vue
@@ -1,15 +1,32 @@
 <template>
-<UserNavbar></UserNavbar>
-<div class="container">
-    
-</div>
-<Userfoot></Userfoot>
+  <UserNavbar></UserNavbar>
+  <div class="container-fluid main">
+    <div class="wrap text-center">
+      <h2 class="text-light text-shadow">成功建立訂單!</h2>
+      <h2 class="text-light text-shadow">祝你成為更好的自己</h2>
+    </div>
+  </div>
 </template>
-<style scoped>
-
+<style scoped lang="scss">
+.text-shadow{
+  text-shadow: 1px 3px 5px rgba(0,0,0,0.6);
+}
+.main {
+  background: rgba(148, 122, 109, 0.6);
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  .wrap{
+    padding-top: 150px;
+    width: 40%;
+    height: 100vh;
+    background: url(https://images.unsplash.com/photo-1535295972055-1c762f4483e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80);
+    background-repeat: no-repeat;
+  }
+}
 </style>
 <script setup>
-import UserNavbar from '../components/Userboard/UserNavbar.vue';
-import Userfoot from '../components/Userboard/Userfoot.vue';
-
+import UserNavbar from "../components/Userboard/UserNavbar.vue";
+import Userfoot from "../components/Userboard/Userfoot.vue";
 </script>
